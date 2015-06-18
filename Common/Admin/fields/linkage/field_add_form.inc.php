6d3b5062b19961defefba5e38efe7776144b23fd
MacXiaoYao <xiaoyao@MacXiaoYaodeMacBook-Pro.local>
2015-06-18 21:15:55 +0800

    联动菜单字段相关属性精简

--- Common/Admin/fields/linkage/field_add_form.inc.php
+++ Common/Admin/fields/linkage/field_add_form.inc.php
@@ -7,23 +7,6 @@
     </td>
   </tr>
   <tr>
-    <td>显示方式</td>
-    <td>
-      <input name="setting[showtype]" value="0" type="radio">
-      只显示名称
-      <input name="setting[showtype]" value="1" type="radio">
-      显示完整路径
-      <input name="setting[showtype]" value="2" type="radio">
-      返回联动菜单id
-      <input name="setting[showtype]" value="3" type="radio">
-      返回菜单层级数组
-    </td>
-  </tr>
-  <tr>
-    <td>路径分隔符</td>
-    <td><input type="text" name="setting[space]" value="" size="5" class="input-text"> 显示完整路径时生效</td>
-  </tr>
-  <tr>
     <td>是否作为筛选字段</td>
     <td>
       <input type="radio" name="setting[filtertype]" value="1"/> 是
--- Common/Admin/fields/linkage/field_edit_form.inc.php
+++ Common/Admin/fields/linkage/field_edit_form.inc.php
@@ -9,23 +9,6 @@
     </td>
   </tr>
   <tr>
-    <td>显示方式</td>
-    <td>
-      <input name="setting[showtype]" value="0" type="radio" <?php if($setting['showtype']==0) echo 'checked';?>>
-      只显示名称
-      <input name="setting[showtype]" value="1" type="radio" <?php if($setting['showtype']==1) echo 'checked';?>>
-      显示完整路径
-      <input name="setting[showtype]" value="2" type="radio" <?php if($setting['showtype']==2) echo 'checked';?>>
-      返回联动菜单id
-      <input name="setting[showtype]" value="3" type="radio" <?php if($setting['showtype']==3) echo 'checked';?>>
-      返回菜单层级数组
-    </td>
-  </tr>
-  <tr>
-    <td>路径分隔符</td>
-    <td><input type="text" name="setting[space]" value="<?php echo $setting['space'];?>" size="5" class="input-text"> 显示完整路径时生效</td>
-  </tr>
-  <tr>
     <td>是否作为筛选字段</td>
     <td>
       <input type="radio" name="setting[filtertype]" value="1" <?php if($setting['filtertype']) echo 'checked';?> /> 是
