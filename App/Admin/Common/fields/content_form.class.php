2d777f9a3db3545d6d17427c4670dc85f355b2c9
XiaoYao <476552238@qq.com>
2015-10-01 15:28:03 +0800

    TP版本升级带来的表操作SQL错误修复

--- App/Admin/Common/fields/content_form.class.php
+++ App/Admin/Common/fields/content_form.class.php
@@ -6,7 +6,7 @@ class content_form {
 	var $formValidator;
 	var $siteid;
 
-	function __construct($modelid,$catid = 0,$categorys = array()) {
+	function __construct($modelid,$catid=0,$categorys = array()) {
 		$this->modelid = $modelid;
 		$this->catid = $catid;
 		$this->categorys = $categorys;
@@ -180,13 +180,13 @@ class content_form {
 		extract($setting);
 		$html = '';
 		if (defined('IN_ADMIN')) {
-			$html = "<input type=\"button\" style=\"width: 66px;\" class=\"button\" onclick=\"crop_cut_".$field."($('#$field').val());return false;\" value=\"裁切图片\"><input type=\"button\" style=\"width: 66px;\" class=\"button\" onclick=\"$('#".$field."_preview').attr('src','".C('TMPL_PARSE_STRING.IMG_PATH')."/admin/icon/upload-pic.png');$('#".$field."').val(' ');return false;\" value=\"取消图片\"><script type=\"text/javascript\">function crop_cut_".$field."(id){ if (id=='') { alert('请先上传缩略图');return false;} window.top.art.dialog({title:'裁切图片', id:'crop', iframe:'index.php?m=content&c=content&a=public_crop&module=content&catid='+catid+'&picurl='+encodeURIComponent(id)+'&input=$field&preview=".($show_type && defined('IN_ADMIN') ? $field."_preview" : '')."', width:'680px', height:'480px'}, 	function(){var d = window.top.art.dialog({id:'crop'}).data.iframe; d.uploadfile();return false;}, function(){window.top.art.dialog({id:'crop'}).close()});};</script>";
+			$html = "<input type=\"button\" style=\"width: 66px;\" class=\"button\" onclick=\"$('#".$field."_preview').attr('src','".C('TMPL_PARSE_STRING.IMG_PATH')."/admin/icon/upload-pic.png');$('#".$field."').val(' ');return false;\" value=\"取消图片\">";
 		}
 		if($show_type && defined('IN_ADMIN')) {
 			$preview_img = $value ? $value : C('TMPL_PARSE_STRING.IMG_PATH').'/admin/icon/upload-pic.png';
 			return $str."<div class='upload-pic img-wrap'><input type='hidden' name='info[$field]' id='$field' value='$value'>
 			<a href='javascript:void(0);' onclick=\"attachupload('{$field}_images', '附件上传','{$field}',thumb_images,'1,{$upload_allowext},$isselectimage,$images_width,$images_height,$watermark','image','".U("Upfile/upload")."');return false;\">
-			<img src='$preview_img' id='{$field}_preview' width='135' height='113' style='cursor:hand' /></a>".$html."</div>";
+			<img src='$preview_img' id='{$field}_preview' width='135' height='113' style='cursor:hand' /></a>" . "<input type=\"button\" style=\"width: 66px;\" class=\"button\" onclick=\"attachupload('{$field}_images', '附件上传','{$field}',thumb_images,'1,{$upload_allowext},$isselectimage,$images_width,$images_height,$watermark','image','".U("Upfile/upload")."');return false;\" value=\"上传图片\">" .$html."</div>";
 		} else {
 			return $str."<input type='text' name='info[$field]' id='$field' value='$value' size='$size' class='input-text' />  <input type='button' class='button' onclick=\"attachupload('{$field}_images', '附件上传','{$field}',thumb_images,'1,{$upload_allowext},$isselectimage,$images_width,$images_height,$watermark','image','".U("Upfile/upload")."');return false;\"/ value='上传图片'>".$html;
 		}
@@ -257,7 +257,7 @@ class content_form {
 		if(empty($position)) return '';
 		$array = array();
 		foreach($position as $_key=>$_value) {
-			if($_value['modelid'] && ($_value['modelid'] !=  $this->modelid) || ($_value['catid'] && strpos(','.$this->categorys[$_value['catid']]['arrchildid'].',',','.$this->catid.',')===false)) continue;
+			if($_value['modelid'] && ($_value['modelid'] !=  $this->modelid || ($_value['catid'] && strpos(','.$this->categorys[$_value['catid']]['arrchildid'].',',','.$this->catid.',')===false))) continue;
 			$array[$_value['id']] = $_value['name'];
 		}
 		$posids = array();
--- App/Admin/Common/fields/edit.sql.php
+++ App/Admin/Common/fields/edit.sql.php
@@ -10,67 +10,67 @@ switch($field_type) {
 		$maxlength = min($maxlength, 255);
 		$fieldtype = $issystem ? 'CHAR' : 'VARCHAR';
 		$sql = "ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` $fieldtype( $maxlength ) NOT NULL DEFAULT '$defaultvalue'";
-		$this->db->query($sql);
+		$this->db->execute($sql);
 	break;
 
 	case 'tinyint':
 		$minnumber = intval($minnumber);
 		$defaultvalue = intval($defaultvalue);
-		$this->db->query("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` TINYINT ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue'");
+		$this->db->execute("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` TINYINT ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue'");
 	break;
 
 	case 'number':
 		$minnumber = intval($minnumber);
 		$defaultvalue = $decimaldigits == 0 ? intval($defaultvalue) : floatval($defaultvalue);
 		$sql = "ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` ".($decimaldigits == 0 ? 'INT' : 'FLOAT')." ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue'";
-		$this->db->query($sql);
+		$this->db->execute($sql);
 	break;
 
 	case 'smallint':
 		$minnumber = intval($minnumber);
 		$defaultvalue = intval($defaultvalue);
-		$this->db->query("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` SMALLINT ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue'");
+		$this->db->execute("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` SMALLINT ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue'");
 	break;
 
 	case 'mediumint':
 		$minnumber = intval($minnumber);
 		$defaultvalue = intval($defaultvalue);
-		$this->db->query("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` MEDIUMINT ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue'");
+		$this->db->execute("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` MEDIUMINT ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue'");
 	break;
 
 
 	case 'int':
 		$minnumber = intval($minnumber);
 		$defaultvalue = intval($defaultvalue);
-		$this->db->query("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` INT ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue'");
+		$this->db->execute("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` INT ".($minnumber >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$defaultvalue'");
 	break;
 
 	case 'mediumtext':
-		$this->db->query("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` MEDIUMTEXT NOT NULL");
+		$this->db->execute("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` MEDIUMTEXT NOT NULL");
 	break;
-	
+
 	case 'text':
-		$this->db->query("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` TEXT NOT NULL");
+		$this->db->execute("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` TEXT NOT NULL");
 	break;
 
 	case 'date':
-		$this->db->query("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` DATE NULL");
+		$this->db->execute("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` DATE NULL");
 	break;
-	
+
 	case 'datetime':
-		$this->db->query("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` DATETIME NULL");
+		$this->db->execute("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` DATETIME NULL");
 	break;
-	
+
 	case 'timestamp':
-		$this->db->query("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` TIMESTAMP NOT NULL");
+		$this->db->execute("ALTER TABLE `$tablename` CHANGE `$oldfield` `$field` TIMESTAMP NOT NULL");
 	break;
 	//特殊自定义字段
 	case 'pages':
-		
+
 	break;
 	case 'readpoint':
 		$defaultvalue = intval($defaultvalue);
-		$this->db->query("ALTER TABLE `$tablename` CHANGE `$oldfield` `readpoint` smallint(5) unsigned NOT NULL default '$defaultvalue'");
+		$this->db->execute("ALTER TABLE `$tablename` CHANGE `$oldfield` `readpoint` smallint(5) unsigned NOT NULL default '$defaultvalue'");
 	break;
 
 }
