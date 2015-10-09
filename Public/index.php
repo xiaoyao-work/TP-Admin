1794505f416d645a728030a2ccefcd6c3e0ff8af
XiaoYao Li <476552238@qq.com>
2015-10-09 16:20:00 +0800

    开启调试模式

--- Public/index.php
+++ Public/index.php
@@ -15,7 +15,7 @@
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',false);
+define('APP_DEBUG',true);
 define('BUILD_DIR_SECURE', false);
 // 定义应用目录
 define('APP_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'App' . DIRECTORY_SEPARATOR);
