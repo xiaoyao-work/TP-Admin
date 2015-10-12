eaa7424fb456786dba323060110b5c7d4b562a77
XiaoYao <476552238@qq.com>
2015-10-12 15:18:39 +0800

    更改脚本名称

--- App/Shell/Controller/IndexController.class.php
+++ App/Shell/Controller/IndexController.class.php
@@ -14,11 +14,11 @@ use Think\Controller;
  * 后台首页
 */
 class IndexController extends Controller {
-    public function v2_1() {
+    public function v2_0_alpha() {
         M()->execute("ALTER TABLE `" . C('DB_PREFIX') . "model_field` ADD `islist` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' AFTER `isadd`;");
     }
 
-    public function v2_2() {
+    public function v2_0_bate() {
         $tables = M()->query("show tables;");
         foreach ($tables as $key => $value) {
             M()->execute("ALTER TABLE `" . $value['tables_in_tp-admin'] . "` ENGINE = INNODB");
