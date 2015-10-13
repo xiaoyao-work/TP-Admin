5f0524334509b4ed354f3073f9a398cdbe456027
XiaoYao <476552238@qq.com>
2015-10-13 18:47:49 +0800

    Taxonomy所需数据库脚本

--- App/Shell/Controller/IndexController.class.php
+++ App/Shell/Controller/IndexController.class.php
@@ -24,4 +24,63 @@ class IndexController extends Controller {
             M()->execute("ALTER TABLE `" . $value['tables_in_'.C('DB_NAME')] . "` ENGINE = INNODB");
         }
     }
+
+    public function v2_1_alpha() {
+        // 创建分类表
+        $taxonomy_table_sql = sprintf("CREATE TABLE `%staxonomy` (
+                    `id` mediumint(8) unsigned primary key AUTO_INCREMENT,
+                    `name` varchar(50),
+                    `description` text,
+                    `siteid` smallint(5) NOT NULL,
+                    `updatetime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+                    `inputtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;", C('DB_PREFIX'));
+        if (M()->execute($taxonomy_table_sql) !== false) {
+            echo "create table taxonomy success!\n";
+        } else {
+            echo "create table taxonomy fail!\n";
+        }
+
+        // 创建目录表
+        $terms_table_sql = sprintf("CREATE TABLE `%sterms` (
+                    `id` mediumint(8) unsigned primary key AUTO_INCREMENT,
+                    `name` varchar(50),
+                    `description` text,
+                    `siteid` smallint(5) NOT NULL,
+                    `updatetime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+                    `inputtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;", C('DB_PREFIX'));
+        if (M()->execute($terms_table_sql) !== false) {
+            echo "create table terms success!\n";
+        } else {
+            echo "create table terms fail!\n";
+        }
+        // 创建分类目录关联表
+        $term_taxonomy_table_sql = sprintf("CREATE TABLE `%sterm_taxonomy` (
+                    `id` mediumint(8) unsigned primary key AUTO_INCREMENT,
+                    `term_id` mediumint(8),
+                    `taxonomy_id` mediumint(8),
+                    `parent` mediumint(8),
+                    `siteid` smallint(5) NOT NULL,
+                    KEY taxonomy_term (taxonomy_id, term_id),
+                    KEY term (term_id)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;", C('DB_PREFIX'));
+        if (M()->execute($term_taxonomy_table_sql) !== false) {
+            echo "create table term_taxonomy success!\n";
+        } else {
+            echo "create table term_taxonomy fail!\n";
+        }
+
+        // 创建目录文章关联表
+        $term_contents_table_sql = sprintf("CREATE TABLE `%sterm_contents` (
+                    `term_taxonomy_id` mediumint(8) unsigned,
+                    `content_id` int(11) unsigned,
+                    primary key (term_taxonomy_id, content_id)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;", C('DB_PREFIX'));
+        if (M()->execute($term_contents_table_sql) !== false) {
+            echo "create table term_contents success!\n";
+        } else {
+            echo "create table term_contents fail!\n";
+        }
+    }
 }
