5c92b263bec3644d2ac00aa803e564811e261779
XiaoYao <476552238@qq.com>
2015-10-13 11:21:45 +0800

    网站路径常量获取方式更新

--- Public/index.php
+++ Public/index.php
@@ -27,15 +27,21 @@ $port      = $_SERVER['SERVER_PORT'];
 $disp_port = ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
 $domain    = $_SERVER['SERVER_NAME'];
 
-$doc_root  = str_replace($_SERVER['SCRIPT_NAME'], '', $_SERVER['SCRIPT_FILENAME']);
-$base_url  = str_replace($doc_root, '', str_replace('\\', '/', __DIR__));
+// 网站相对路径
+$script_name_info = pathinfo($_SERVER['SCRIPT_NAME']);
+$base_url = ($script_name_info['dirname'] == DIRECTORY_SEPARATOR ? '' : $script_name_info['dirname']);
+
+// Apache Or Nginx 配置根目录
+$script_filename_info = pathinfo($_SERVER['SCRIPT_FILENAME']);
+$doc_root  = str_replace($base_url, '', $script_filename_info['dirname']);
+
+// 网站完整首页URL
 $full_url  = "{$protocol}://{$domain}{$disp_port}{$base_url}";
 define("ROOT_PATH", __DIR__ . DIRECTORY_SEPARATOR);
 define("DOC_PATH", $doc_root . DIRECTORY_SEPARATOR);
 define("BASE_URL", $base_url . '/');
 define("UPLOAD_URL", $full_url . '/Uploads/');
 define("UPLOAD_PATH", ROOT_PATH . 'Uploads' . DIRECTORY_SEPARATOR);
-
 // 引入ThinkPHP入口文件
 require '../ThinkPHP/ThinkPHP.php';
 
