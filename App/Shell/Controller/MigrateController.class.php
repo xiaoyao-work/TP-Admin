4c261549ecc3f106ab3d8da4fb1e7468b1b0aeff
XiaoYao <476552238@qq.com>
2015-10-27 22:11:53 +0800

    权限控制升级

--- App/Shell/Controller/MigrateController.class.php
+++ App/Shell/Controller/MigrateController.class.php
@@ -15,10 +15,14 @@ use Think\Controller;
 */
 class MigrateController extends Controller {
     public function addColumnRequestMethodForNode() {
-        if (M()->execute("ALTER TABLE `" . C('DB_PREFIX') . "node` ADD `request_method` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' comment '请求方式；0: ALL,1:GET,2::POST' AFTER `params`;") !== 'false') {
+        if (M()->execute("ALTER TABLE `" . C('DB_PREFIX') . "node` ADD `request_method` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' comment '是否验证请求方式；0: 否,1:是' AFTER `params`;") !== false) {
             echo "执行成功";
         } else {
             echo "执行失败";
         }
+    }
+
+    public function addColumnRequestMethodForAccess() {
+        echo M()->execute("ALTER TABLE `" . C('DB_PREFIX') . "access` ADD `request_method` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0' comment '请求方式；0: All, 1: Get 2: Post' AFTER `siteid`;") === false ? '执行失败' : '执行成功';
     }
 }
